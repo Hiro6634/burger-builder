b01e19f81ff82a3fcabfd40234f8cde52a9fc415
suyamaed <suyama_eduardo@redlink.com.ar>
2020-11-20 18:41:59 -0300

    222. Setting Up Routing & Routes

--- src/App.js
+++ src/App.js
@@ -2,24 +2,17 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-  // state = {
-  //   show:true
-  // }
-
-  // componentDidMount(){
-  //   setTimeout(() => {
-  //     this.setState({show:false});
-  //   }, 5000);
-  // }
   render() {
     return (
       <div>
         <Layout>
-          {/* {this.state.show? <BurguerBuilder />: null} */}
-          <BurguerBuilder />
-          <Checkout/>
+          <Switch>
+            <Route path="/checkout" component={Checkout}/>
+            <Route path="/" component={BurguerBuilder}/>
+          </Switch>
         </Layout>
       </div>
     );
