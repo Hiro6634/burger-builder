3a2c89e7b61423de553f6b01a642bae110f3f7e7
suyamaed <suyama_eduardo@redlink.com.ar>
2020-11-30 01:47:25 -0300

    227. Order Submission & Passing Data Between Pages

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -4,9 +4,9 @@ import Burger from  '../../components/Burger/Burguer';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import axios from '../../axios-orders';
 
 const INGRIDENT_PRICES = {
     salad: 0.5,
@@ -91,35 +91,11 @@ class BurguerBuilder extends Component{
     }
 
     purchaseContiueHandler = () => {
-        // // alert('You continue!');
-        // this.setState({loading: true});
-        // const order = {
-        //     ingrediens: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Max',
-        //         address: {
-        //             street: 'Calle florida',
-        //             zipCode: '1234',
-        //             country: 'republica bananera'
-        //         },
-        //         email: 'max@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-
-        // axios.post('/orders.json', order)
-        //     .then( response => {
-        //         this.setState({loading: false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading: false, purchasing: false})
-        //         console.log(error)});
         const queryParams = [];
         for( let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push(
             {
