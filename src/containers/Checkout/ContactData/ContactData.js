676c785946c7a139882123efa248a744c1f5c917
suyamaed <suyama_eduardo@redlink.com.ar>
2020-12-17 13:16:35 -0300

    346. Sharing the Validation Method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,7 +6,7 @@ import Input from '../../../components/UI/Input/Input';
 import { connect } from 'react-redux';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 import classes from './ContactData.css';
 class ContactData extends Component {
@@ -116,28 +116,11 @@ class ContactData extends Component {
         this.props.onOrderBurger(order, this.props.token);
 
     }
-
-    checkValidation( value, rules){
-        let isValid = true;
-
-        if( rules.required){
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if( rules.minLength){
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if( rules.maxLength ) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-        return  isValid;
-    }
-
+ 
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier],{
             value: event.target.value,
-            valid: this.checkValidation( event.target.value, this.state.validation),
+            valid: checkValidity( event.target.value,  this.state.orderForm[inputIdentifier].validation),
             touched: true
         });
 
